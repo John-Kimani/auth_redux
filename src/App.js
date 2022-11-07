07d8f9ad2a753e444a16058e74103b59afaabeba
John-Kimani <kimwaweru100@gmail.com>
2022-11-07 11:40:35 +0300

    Dev: create to to handle login user slice

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import LoginPage from './pages/auth/LoginPage';
 import HomePage from './pages/homepage/HomePage';
 
 
+import {Provider} from 'react-redux';
+import store from "./app/store";
+
+
 function App() {
   return (
     <>
-      <BrowserRouter>
+    <Provider store={store}>
+    <BrowserRouter>
         <Routes>
           <Route path='/' exact element={<HomePage />}/>
           <Route path='/login' element={<LoginPage />}/>
         </Routes>
       </BrowserRouter>
+    </Provider>
     </>
   );
 }
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,23 +1,57 @@
-import React, { useState } from 'react';
-import './login.css'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../features/userSlice";
+import "./login.css";
 
 const Login = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  return (
-    <section className='container section'>
-        <div className="login__interface">
-            <form className="login__form">
-                <h2 className='form__title'>Login Here</h2>
-                <input type="name" placeholder='Name' value={name} onChange={ (e) => setName(e.target.value)}/>
-                <input type="email" placeholder='Email' value={email} onChange={ (e) => setEmail(e.target.value)}/>
-                <input type="password" placeholder='Password' value={password} onChange={ (e) => setPassword(e.target.value)}/>
-                <button type='submit' className='submit__button'>Login</button>
-            </form>
-        </div>
-    </section>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(
+      login({
+        name: name,
+        email: email,
+        password: password,
+        loggedIn: true,
+      })
+    );
+  };
+  return (
+    <section className="container section">
+      <div className="login__interface">
+        <form className="login__form" onSubmit={(e) => handleSubmit(e)}>
+          <h2 className="form__title">Login Here</h2>
+          <input
+            type="name"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="submit__button">
+            Login
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
