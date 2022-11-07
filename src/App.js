7c822e5edac5f6a1e662ecb8f139bc6f39395774
John-Kimani <kimwaweru100@gmail.com>
2022-11-07 17:01:25 +0300

    Dev: Move store provider, wraps App instance index file

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import LoginPage from './pages/auth/LoginPage';
 import HomePage from './pages/homepage/HomePage';
 
 
-import {Provider} from 'react-redux';
-import store from "./app/store";
+
 
 
 function App() {
   return (
     <>
-    <Provider store={store}>
     <BrowserRouter>
         <Routes>
           <Route path='/' exact element={<HomePage />}/>
           <Route path='/login' element={<LoginPage />}/>
         </Routes>
       </BrowserRouter>
-    </Provider>
     </>
   );
 }
