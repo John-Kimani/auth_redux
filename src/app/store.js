d75cf80c0e2f3cba9e03a055637cb548d5ce62a9
John-Kimani <kimwaweru100@gmail.com>
2022-11-07 17:09:46 +0300

    Dev: Define cart actions as reducer element

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "../features/cartSlice";
 import userReducer from "../features/userSlice";
 
 export default configureStore({
     reducer: {
         user: userReducer,
+        cart: cartSlice,
     }
 })
