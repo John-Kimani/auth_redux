c05f88ddda5f775adeef7722a7f6fab3fb81433c
John-Kimani <kimwaweru100@gmail.com>
2022-11-10 13:52:34 +0300

    Dev: Homepage responsiveness

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,50 +1,58 @@
-import React from 'react';
+import React from "react";
 import "./home.css";
-import { pizzaMenu } from './Data';
-import { useDispatch } from 'react-redux';
-import { addItem } from '../../features/cartSlice';
+import { pizzaMenu } from "./Data";
+import { useDispatch } from "react-redux";
+import { addItem } from "../../features/cartSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  // const [item, setItem ] = useState({});
 
-    // const [item, setItem ] = useState({});
-
-    const handleAddToCart = () => {
-        dispatch(
-            addItem({
-                id: 3,
-                pizza_name: "Meat Deluxe",
-                quantity: 10,
-                price: 570,
-                description: "For meat lovers, taste variant from beef to mutton.",
-            })
-        );
-    };
+  const handleAddToCart = () => {
+    dispatch(
+      addItem({
+        id: 3,
+        pizza_name: "Meat Deluxe",
+        quantity: 10,
+        price: 570,
+        description: "For meat lovers, taste variant from beef to mutton.",
+      })
+    );
+  };
 
   return (
-    <section className="section home">
-        <div className="home__container">
-            <div className="home__content container grid">
-                {pizzaMenu.map(({id, pizza_name, quantity, description, price}) => {
-                    return(
-                        <div className="item__card" key={id}>
-                            <h3 className="item__card-title">{pizza_name}</h3>
-                            <div className="item__card-description">
-                                <p>{description}</p>
-                                <p>In Stock: {quantity}</p>
-                                <p>Price: {price}</p>
-                            </div>
-                            <div className="item__card-footer">
-                                <button className='cart__button' onClick={handleAddToCart}>Add to cart</button>
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
+    <section className="container section home">
+              <div className="home__content">
+          {pizzaMenu.map(
+            ({ id, image, pizza_name, quantity, description, price }) => {
+              return (
+                <div className="item__card" key={id}>
+                  <div className="card__infomation">
+                    <div className="card__image">
+                        <img src={image} alt={pizza_name} />
+                    </div>
+                    <div className="item__description">
+                      <h3 className="item__card-title">{pizza_name}</h3>
+                      <div className="item__card-description">
+                        <p>{description}</p>
+                        <p>Price: {price}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item__card-footer">
+                    <p>In Stock: {quantity}</p>
+                    <button className="cart__button" onClick={handleAddToCart}>
+                      Add to cart
+                    </button>
+                  </div>
+                </div>
+              );
+            }
+          )}
         </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
