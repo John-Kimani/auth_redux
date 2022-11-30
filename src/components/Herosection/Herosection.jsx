7c3028b11089988480825bcb6ac17e372d6081e1
John-Kimani <kimwaweru100@gmail.com>
2022-11-30 14:42:16 +0300

    Fix: Hero section responsiveness

--- src/components/Herosection/Herosection.jsx
+++ src/components/Herosection/Herosection.jsx
@@ -4,23 +4,18 @@ import HeroSectionImage from '../../assets/chicken_with_coke.jpg'
 
 function Herosection() {
   return (
-    <div className='hero__section'>
-        <div className="container">
-          <div className="hero__section-content">
-            <div className="hero__section-introduction">
-                <h1>Pizza MAMA is an online application enabling users to view our restaurant's menu.</h1>
-                <q>Beauty comes in all shapes and sizes. Small, large, square, thin crust, stuffed crust, extra toppings.</q> ~ <cite>Anonymous</cite>
+    <div className="container hero_section">
+      <div className="grid section">
+            <div className="hero_section-introduction">
+              <h1>Pizza MAMA</h1>
+              <p className='paragraph'>Pizza MAMA is an online application enabling authenticated users to view our restaurant's menu.</p>
+              <q className='quote'>
+              Beauty comes in all shapes and sizes. Small, large, square, thin crust, stuffed crust, extra toppings.</q> ~ <cite className='cite'>Anonymous</cite>
             </div>
-            <div className="hero__section-image">
-              <img src={HeroSectionImage} alt='hero-section'/>
-              <h3>Order Online</h3>
-              <p>Simple and convient just on your phone.</p>
-                <br />
-              <h3>View menu</h3>
-              <p>Explore our wide range of delicious and healthy food items</p>
+            <div className="hero_section-image">
+              <img src={HeroSectionImage} alt="hero-section" />
             </div>
-          </div>
-        </div>
+      </div>
     </div>
   )
 }
