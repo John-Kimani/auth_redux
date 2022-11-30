e57d6933fb47a33134c7cd0266ec79f05b210b59
John-Kimani <kimwaweru100@gmail.com>
2022-11-30 15:03:19 +0300

    Fix: Menu section responsiveness

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,11 +22,40 @@ const Home = () => {
   };
 
   return (
-    <section className="container menu__section home">
+    <section className="container menu__section ">
       <div className="section__title">
         <h1>Our Menu</h1>
       </div>
-              <div className="home__content">
+
+      <div className="menu__section-content grid">
+      {pizzaMenu.map(
+            ({ id, image, pizza_name, quantity, description, price }) => {
+              return (
+                <div className="item__card" key={id}>
+                  <div className="card__infomation">
+                    <div className="card__image">
+                        <img src={image} alt={pizza_name} />
+                    </div>
+                    <div className="item__description">
+                      <h3 className="item__card-title">{pizza_name}</h3>
+                      <div className="item__card-description">
+                        <p>{description}</p>
+                        <p>Price: {price}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item__card-footer">
+                    <p>In Stock: {quantity}</p>
+                    <button className="cart__button" onClick={handleAddToCart}>
+                      Add to cart
+                    </button>
+                  </div>
+                </div>
+              );
+            }
+          )}
+      </div>
+              {/* <div className="home__content">
           {pizzaMenu.map(
             ({ id, image, pizza_name, quantity, description, price }) => {
               return (
@@ -53,7 +82,7 @@ const Home = () => {
               );
             }
           )}
-        </div>
+        </div> */}
     </section>
   );
 };
